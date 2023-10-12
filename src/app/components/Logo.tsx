36f0de4fdbb56de1bcef1b3a2fd958b1ec17d986
JackWinterburn <52929068+JackWinterburn@users.noreply.github.com>
2023-10-12 08:45:45 +0000

    fixed quote issue with logo component

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -2,6 +2,6 @@ import { Box } from "@chakra-ui/react"
 
 export default function Logo() {
     return (
-        <Box bg="white" p="1em  ">"LOGO"</Box>
+        <Box bg="white" p="1em  ">&QUOTELOGO&QUOTE"</Box>
     );
 }
